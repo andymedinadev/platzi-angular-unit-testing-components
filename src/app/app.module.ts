8e69b264a7ecfaeb02096cb90d735db2e0576ef1
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-04-08 14:37:07 -0300

    chore: add ReversePipe to do testing on Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { PeopleComponent } from './components/people/people.component';
 import { ProductComponent } from './components/product/product.component';
 import { OthersComponent } from './components/others/others.component';
 import { HighlightDirective } from './directives/highlight.directive';
+import { ReversePipe } from './pipes/reverse.pipe';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { HighlightDirective } from './directives/highlight.directive';
     ProductComponent,
     OthersComponent,
     HighlightDirective,
+    ReversePipe,
   ],
   imports: [
     BrowserModule,
